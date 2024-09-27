632c8c8bc903b93b9c6b04cd709bd6186b8b54af
Daniel <d15_1_89@msn.com>
2024-09-27 13:25:52 +0200

    no needed routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
 import { MortgageFormModule } from './mortgage-form/mortgage-form.module';
@@ -18,15 +11,9 @@ import { MortgageResultModule } from './mortgage-result/mortgage-result.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     MortgageFormModule,
     MortgageResultModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {
-    path: 'mortgage-form',
-    loadChildren: () =>
-      import('./mortgage-form/mortgage-form.module').then(
-        (m) => m.MortgageFormModule
-      ),
-  },
-  {
-    path: 'mortgage-result',
-    loadChildren: () =>
-      import('./mortgage-result/mortgage-result.module').then(
-        (m) => m.MortgageResultModule
-      ),
-  },
-  {
     path: '',
-    redirectTo: '/mortgage-form',
-    pathMatch: 'full',
+    component: AppComponent,
   },
   {
     path: '**',
-    redirectTo: '/mortgage-form',
+    redirectTo: '',
   },
 ];
